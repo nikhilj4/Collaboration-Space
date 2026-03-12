6628e2a545d032df1695fe55978274a4af9b1deb
Nikhil J <nikhiljram4@gmail.com>
2026-03-12 15:14:44 +0530

    fix: avoid cert crashing during build time by supplying dummy credentials

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -15,11 +15,11 @@ function getAdminApp(): App {
 
     return initializeApp({
         credential: cert({
-            projectId: process.env.FIREBASE_PROJECT_ID!,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')!,
+            projectId: process.env.FIREBASE_PROJECT_ID || 'dummy-project',
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL || 'dummy@dummy.com',
+            privateKey: (process.env.FIREBASE_PRIVATE_KEY || '-----BEGIN PRIVATE KEY-----\ndummy\n-----END PRIVATE KEY-----\n').replace(/\\n/g, '\n'),
         }),
-        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || 'dummy.appspot.com',
     });
 }
 
